e94a424bcd68136abf72ec75bba5f2580fa8989b
Xinyang Wen <927597@ACDM.DS.SAIT.CA>
2024-01-18 09:18:24 -0700

    day04-styles

---
+++ components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <div>
+      <NuxtLink to="/">Logo: User Name</NuxtLink>
+    </div>
+    <nav>
+      <NuxtLink to="signup">Sign Up</NuxtLink>
+    </nav>
+  </header>
+</template>
+
+<style>
+header {
+  display: flex;
+  justify-content: space-between;
+  gap: 2rem;
+  padding: 2rem;
+  color: white;
+  background-color: black;
+}
+a {
+  color: white;
+}
+a:hover {
+  color: blue;
+}
+</style>
